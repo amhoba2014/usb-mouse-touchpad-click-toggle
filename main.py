26fc494d106ee7ea5fb7e66642f09d67c4c92235
Amir Hossein Baghernezhad <amhoba2014@gmail.com>
2024-11-11 22:44:46 +0330

    basic setup stuff

--- main.py
+++ main.py
@@ -7,10 +7,50 @@ from source.xinput_utils import XinputUtils
 def main():
   configs = ConfigManager.read_env_file()
   if configs is None:
-    # configs is None so this means we should perform setup.
+    # The `configs` variable` is None so this means we should perform setup:
+    print("Let's perform a setup.")
     xinput_devices = XinputUtils.extract_xinput_devices()
     xinput_pointer_devices = [device for device in xinput_devices if device.device_type == "pointer"]
-    print(xinput_pointer_devices)
+    # Choose USB mouse device:
+    print()
+    print("Please choose the USB mouse device from the list below (example: 'PixArt USB Optical Mouse'):")
+    for i, x in enumerate(xinput_pointer_devices):
+      print(i, x.name)
+    index = int(input("Enter the numeric index: "))
+    if index < 0 or index >= len(xinput_pointer_devices):
+      print("Please choose from the list!")
+      return
+    chosen_usb_mouse_device = xinput_pointer_devices[index]
+    print("The chosen device is:", chosen_usb_mouse_device.name)
+    # Choose touchpad device:
+    print()
+    print("Please choose the Touchpad device from the list below (example: 'SynPS/2 Synaptics TouchPad'):")
+    for i, x in enumerate(xinput_pointer_devices):
+      print(i, x.name)
+    index = int(input("Enter the numeric index: "))
+    if index < 0 or index >= len(xinput_pointer_devices):
+      print("Please choose from the list!")
+      return
+    chosen_touchpad_device = xinput_pointer_devices[index]
+    print("The chosen device is:", chosen_touchpad_device.name)
+    # Choose the suitable prop:
+    print()
+    print("Now please carefully choose the prop of your touchpad that determines if tapping is enabled or not (example: 'libinput Tapping Enabled'):")
+    touchpad_device_properties = XinputUtils.get_device_properties(chosen_touchpad_device.device_id)
+    for i, x in enumerate(touchpad_device_properties):
+      print(i, x.name)
+    index = int(input("Enter the numeric index: "))
+    if index < 0 or index >= len(touchpad_device_properties):
+      print("Please choose from the list!")
+      return
+    chosen_touchpad_device_property = touchpad_device_properties[index]
+    print("The chosen device property is:", chosen_touchpad_device_property.name)
+
+
+
+
+
+
     return
 
   while True:
